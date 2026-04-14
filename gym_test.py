0559bc1d937f2a5a6520d3579a04f840be07affa
RyanAspen <ryanaspen27@gmail.com>
2026-04-13 21:24:13 -0400

    Removed code that would not work

--- gym_test.py
+++
@@ -1,8 +0,0 @@
-import sheepshead_v1
-from gym_utils import train_action_mask, eval_action_mask
-
-env_fn = sheepshead_v1
-
-env_kwargs = {}
-
-train_action_mask(env_fn, steps=10_240, seed=0, **env_kwargs)
